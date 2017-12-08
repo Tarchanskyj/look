4957cbb1932fc86cc4aaf2fae307a1b38aa4cb2e
Arthur <arthurka.company@gmail.com>
2017-12-09 01:42:23 +0200

    Add view on site

--- product/admin.py
+++ product/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.urls import reverse
+
 from product.models import Product
 
 
-admin.site.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    def view_on_site(self, obj):
+        return reverse('product:product_detail',
+                                              kwargs={'slug': obj.slug})
+
+
+
+admin.site.register(Product, ProductAdmin)
--- product/models.py
+++ product/models.py
@@ -13,6 +13,16 @@ class Product(models.Model):
         return self.name
 
 
+class Comment(models.Model):
+    author = models.CharField(max_length=255)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey('Product')
+
+    def __str__(self):
+        return self.author + ' - ' + self.product.name
+
+
 
 
 # Create your models here.
